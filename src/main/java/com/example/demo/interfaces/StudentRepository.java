2c2335cd623151f14745311d50edf58357901621
Ruben Contreras <ruben4tip@hotmail.com>
2022-09-02 02:41:22 -0400

    se agregan consultas personalizadas al repositorio
    se muestran dos formas de hacer las consultas en el repositorio, una es simplemente usando springboot para que el sepa como hace la consulta, la otra es de una forma un poco mas personalizada pero aun usando la consulta conmbinada con sl framework,. existe una tercera forma que no se muestra y es con un procedimiento almacenado e indicando que cosa respondera la BD

--- src/main/java/com/example/demo/interfaces/StudentRepository.java
+++ src/main/java/com/example/demo/interfaces/StudentRepository.java
@@ -2,10 +2,29 @@ package com.example.demo.interfaces;
 
 import com.example.demo.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // T es la clase que se utilizara del modelo y ID es la primary key
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    /** 1 Forma de hacerlo
+     * es igual que SELECT * FROM STUDENT.STUDENTS WHERE EMAIL = ?;
+     * @param email email
+     * @return student
+     */
+    Optional<Student> findStudentByEmail(String email);
+
+    /** 2 Forma de hacerlo
+     * las "s" hace referencia a que Student y como Student está mapeado a la BD
+     * SpringBoot sabe como hacer la consulta
+     * @param email email
+     * @return student
+     */
+    @Query("select s from Student s where s.email =?1")
+    Optional<Student> findStudentXEmail(String email);
 
 }
