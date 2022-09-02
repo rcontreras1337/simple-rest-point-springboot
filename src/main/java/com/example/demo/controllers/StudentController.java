dc187de40976b7cdee099505f737670691fee18e
Ruben Contreras <ruben4tip@hotmail.com>
2022-09-01 23:19:09 -0400

    se inyecta la depencia y se agregan anotaciones Bean der servicio
    se inyecta la independiecnia de forma correcta con el autowired y se anota tambien el servicio como @Service para que sea reconocido

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -2,22 +2,33 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Student;
 import com.example.demo.services.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controlador para los endpoint de Student
+ */
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
     private final StudentServiceImpl studentService;
 
+    /**
+     * @param studentService
+     */
+    @Autowired
     public StudentController(StudentServiceImpl studentService) {
-        this.studentService = new StudentServiceImpl();
+        this.studentService = studentService;
     }
 
+    /**
+     * @return List<Student>
+     */
     @GetMapping(path = "getStudents")
     public List<Student> getStudents(){
         return studentService.getStudents();
--- src/main/java/com/example/demo/services/StudentServiceImpl.java
+++ src/main/java/com/example/demo/services/StudentServiceImpl.java
@@ -1,14 +1,22 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Student;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Servicio que se usara por el controlador para la generacion del endpoint de Student
+ */
+@Service
 public class StudentServiceImpl {
 
+    /** metodo que obtiene la lista de estudiantes
+     * @return List<Student>
+     */
     public List<Student> getStudents(){
         return Arrays.asList(
             new Student(
