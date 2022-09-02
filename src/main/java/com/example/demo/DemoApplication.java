809f1fa5f047c7beedade1e446445a145ca779c7
Ruben Contreras <ruben4tip@hotmail.com>
2022-09-01 22:14:23 -0400

    Endpoint devuelve ahora un Array de objetos

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.models.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,7 +21,15 @@ public class DemoApplication {
     }
 
     @GetMapping
-    public List<String> helloWorld(){
-        return Arrays.asList("Hello"," world","loco");
+    public List<Student> helloWorld(){
+        return Arrays.asList(
+            new Student(
+                1L,
+                "Ruben",
+                "ruben4tip@gmail.com",
+                LocalDate.of(1994, Month.AUGUST,17),
+                (short) 28
+            )
+        );
     }
 }
